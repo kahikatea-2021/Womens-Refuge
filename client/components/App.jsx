cade287f1b18daaea425aeba2d0c60dd42816b71
jared-pinfold <j.pinfold@gmail.com>
2021-05-05 19:16:38 +1200

    commented out all of the Fruits related preset stuff in the boilerplate

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,30 +1,30 @@
-import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
+// import React, { useEffect } from 'react'
+// import { connect } from 'react-redux'
 
-import { fetchFruits } from '../actions'
+// import { fetchFruits } from '../actions'
 
-function App (props) {
-  useEffect(() => {
-    props.dispatch(fetchFruits())
-  }, [])
+// function App (props) {
+//   useEffect(() => {
+//     props.dispatch(fetchFruits())
+//   }, [])
 
-  return (
-    <>
-      <div className='app'>
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>
-          {props.fruits.map(fruit => (
-            <li key={fruit}>{fruit}</li>
-          ))}
-        </ul>
-      </div>
-    </>
-  )
-}
-const mapStateToProps = (globalState) => {
-  return {
-    fruits: globalState.fruits
-  }
-}
+//   return (
+//     <>
+//       <div className='app'>
+//         <h1>Fullstack Boilerplate - with Fruits!</h1>
+//         <ul>
+//           {props.fruits.map(fruit => (
+//             <li key={fruit}>{fruit}</li>
+//           ))}
+//         </ul>
+//       </div>
+//     </>
+//   )
+// }
+// const mapStateToProps = (globalState) => {
+//   return {
+//     fruits: globalState.fruits
+//   }
+// }
 
-export default connect(mapStateToProps)(App)
+// export default connect(mapStateToProps)(App)
