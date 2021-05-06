f58d1267729b998a360211c5b704bdbe438d849d
Erin L Abernethy <40452106+ErinLlouise@users.noreply.github.com>
2021-05-06 15:41:19 +1200

    add new route.js and fix some component linting

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 
-function Login() {
-    return (
-        <>
-        <Header />
-        <h2>Kia Ora!</h2>
-        <h4>Please log in:</h4>
-        <form>
-        <label for="username">Username: </label>
+function Login () {
+  return (
+    <>
+      <Header />
+      <h2>Kia Ora!</h2>
+      <h4>Please log in:</h4>
+      <form>
+        <label htmlFor="username">Username: </label>
         <input type='text'></input>
         <br />
-        <label for="password">Password: </label>
+        <label htmlFor="password">Password: </label>
         <input type='text'></input>
-        </form>
-        <Footer />
-        </>
-    )
+      </form>
+      <Footer />
+    </>
+  )
 }
 
 export default Login
