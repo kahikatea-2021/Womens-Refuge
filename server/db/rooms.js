714af07e6d4f5aeee8036776e9e9dd7b02ce0c41
Rebecca Hallas <77651862+rebecca-hallas@users.noreply.github.com>
2021-05-07 10:36:38 +1200

    added db functions

--- server/db/rooms.js
+++ server/db/rooms.js
@@ -1,41 +1,36 @@
 const connection = require('./connection')
 
 // Get every house in the database
-function getRoomById (db = connection) {
-  return db('room')
+function getRoomById (id, db = connection) {
+  return db('rooms')
     .where('id', '=', id)
 }
 
 // when a user searches for a specific house name, that house is returned
-function getHouseByName (name, db = connection) {
+function getRoomsByHouse (name, db = connection) {
   return db('houses')
     .where('name', '=', name)
 }
 
-// when a user clicks on a house to view the house details
-function getHouseById (id, db = connection) {
-  return db('houses')
-    .where('id', '=', id)
+function updateRoomDescription (roomId, updateDescription, db = connection) {
+  return db('rooms')
+    .where('id', '=', roomId)
+    .update({
+      description: updateDescription
+    })
 }
 
-// Get every house in a region houses
-function getAllRegionalHouses (region, db = connection) {
-  return db('houses')
-    .join('regions', 'regions.id', 'houses.region_id')
-    .where('region', '=', region)
-}
-
-// exclude houses from selected regions
-function excludeRegions (regions, db = connection) {
-  return db('regions')
-    .join('houses', 'regions.id', 'region_id')
-    .whereNotIn('region', regions)
+function updateRoomAvailability (roomId, updateAvailable, db = connection) {
+  return db('rooms')
+    .where('id', '=', roomId)
+    .update({
+      available: updateAvailable
+    })
 }
 
 module.exports = {
-  getAllHouses,
-  getAllRegionalHouses,
-  getHouseById,
-  getHouseByName,
-  excludeRegions
+  getRoomsByHouse,
+  getRoomById,
+  updateRoomDescription,
+  updateRoomAvailability
 }
