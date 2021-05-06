49ff1df5713a69ee467e1af222a45ca6114b5222
Jared Pinfold <77950295+jared-pinfold@users.noreply.github.com>
2021-05-06 15:20:39 +1200

    created region seed data

--- server/db/seeds/fruit.js
+++ server/db/seeds/fruit.js
@@ -1,12 +1,12 @@
-// exports.seed = function (knex) {
-//   // Deletes ALL existing entries
-//   return knex('fruit').del()
-//     .then(function () {
-//       // Inserts seed entries
-//       return knex('fruit').insert([
-//         { id: 1, name: 'banana' },
-//         { id: 2, name: 'apple' },
-//         { id: 3, name: 'feijoa' }
-//       ])
-//     })
-// }
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        { id: 1, email: 'admin@example.org', password: '21', isAdmin: true, isHost: false },
+        { id: 2, email: 'host@example.org', password: '21', isAdmin: false, isHost: true },
+        { id: 2, email: 'police@example.org', password: '21', isAdmin: false, isHost: false }
+      ])
+    })
+}
