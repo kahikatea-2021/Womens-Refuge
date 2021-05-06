a46ee14e5d2435ede2893391ea3a952195a5f1e8
Grace Malae <77918333+grace-malae@users.noreply.github.com>
2021-05-07 11:21:39 +1200

    updated house id to user table

--- server/db/migrations/20190905120752_users.js
+++ server/db/migrations/20190905120752_users.js
@@ -14,6 +14,7 @@ exports.up = function (knex) {
     table.string('email')
     table.boolean('isMasterAdmin')
     table.boolean('isRefugeCoordinator')
+    table.int('house_id').references('houses.id')
   })
 }
 
--- server/db/seeds/users.js
+++ server/db/seeds/users.js
@@ -4,9 +4,9 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        { id: 'auth0|609471b258f57300728a1357', email: 'masteradmin@safehouse.com', isMasterAdmin: true, isRefugeCoordinator: false },
-        { id: 'auth0|6094708749cdbd006b842fa3', email: 'refugecoordinator@safehouse.com', isMasterAdmin: false, isRefugeCoordinator: true },
-        { id: 'auth0|6094723849cdbd006b842fc5', email: 'generaluser@safehouse.com', isMasterAdmin: false, isRefugeCoordinator: false }
+        { id: 'auth0|609471b258f57300728a1357', email: 'masteradmin@safehouse.com', isMasterAdmin: true, isRefugeCoordinator: false, house_id: null },
+        { id: 'auth0|6094708749cdbd006b842fa3', email: 'refugecoordinator@safehouse.com', isMasterAdmin: false, isRefugeCoordinator: true, house_id: 1 },
+        { id: 'auth0|6094723849cdbd006b842fc5', email: 'generaluser@safehouse.com', isMasterAdmin: false, isRefugeCoordinator: false, house_id: null }
 
       ])
     })
