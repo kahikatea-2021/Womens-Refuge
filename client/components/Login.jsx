818a082620bef2545f2622ccd5b9838be58ce8dc
Jared Pinfold <77950295+jared-pinfold@users.noreply.github.com>
2021-05-06 16:57:55 +1200

    implemented login button

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,23 +1,18 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import LoginButton from './Buttons/LoginButton'
 
-function Login() {
-    return (
-        <>
-        <Header />
-        <h2>Kia Ora!</h2>
-        <h4>Please log in:</h4>
-        <form>
-        <label for="username">Username: </label>
-        <input type='text'></input>
-        <br />
-        <label for="password">Password: </label>
-        <input type='text'></input>
-        </form>
-        <Footer />
-        </>
-    )
+function Login () {
+  return (
+    <>
+      <Header />
+      <h2>Kia Ora!</h2>
+      <h4>Please log in:</h4>
+      <LoginButton />
+      <Footer />
+    </>
+  )
 }
 
 export default Login
