da6fbcd937343ff9f6405755df4113b951edcdca
Rebecca-Hallas <rebeccajhallas@gmail.com>
2021-05-07 21:19:32 +1200

    working on getting individual house page

--- client/components/House.jsx
+++ client/components/House.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import getHouseByName from '../apis/houses'
+// import { useParams } from 'react-router-dom'
 
 function House () {
+const [house, setHouse] = useState([])
+// const houseName = useParams().name
+useEffect(() => {
+  getHouseByName()
+  .then(results => {
+    setHouse(results)
+    return null
+  })
+  .catch(err => console.log(err))
+}, [])
+
   return (
     <>
-      <h1>House Name</h1>
+      <h1>House:</h1>
+      <p key={house.name}>{house.name}</p>
     </>
   )
 }
--- server/routes/houses.js
+++ server/routes/houses.js
@@ -13,7 +13,6 @@ router.get('/', (req, res) => {
   console.log('regions: ', regions)
   houseDb.genearlQuery(island, regions, excludedRegions)
     .then(result => {
-      console.log('in general query', result)
       res.status(200).json(result)
       return null
     })
