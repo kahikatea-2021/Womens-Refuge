17cb372ca6d0a907c9e85bf6cc2b1b29040f82a0
Erin L Abernethy <40452106+ErinLlouise@users.noreply.github.com>
2021-05-07 15:23:46 +1200

    add HouseList.jsx

--- client/components/HouseList.jsx
+++ client/components/HouseList.jsx
@@ -1,1 +1,9 @@
 import React from 'react'
+
+function HouseList () {
+  return (
+    <p>Hi there</p>
+  )
+}
+
+export default HouseList
--- client/components/SouthIsland.jsx
+++ client/components/SouthIsland.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
-import Region from './Region'
+import HouseList from './HouseList'
 
 function SouthIsland () {
   return (
     <>
       <h1>South Island Regions:</h1>
-      <Region />
+
+      <HouseList/>
 
     </>
   )
