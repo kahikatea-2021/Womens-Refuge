2aa93afbbda612068c91264d5932e47c29baf47c
Rebecca Hallas <77651862+rebecca-hallas@users.noreply.github.com>
2021-05-07 18:49:14 +1200

    added tests for get house by id

--- server/db/houses.test.js
+++ server/db/houses.test.js
@@ -15,7 +15,27 @@ beforeEach(() => {
 test('generalQuery should return all the houses by default', () => {
   return houseDb.genearlQuery('all', [], [], testDb)
     .then(houses => {
-      expect(houses).toHaveLength(51)
+      expect(houses).toHaveLength(50)
+      return null
+    })
+})
+
+test('addHouse should add a house', () => {
+  const house = { name: 'test house', notes: 'test notes', phone_1: 12345, phone_2: 45678, region_id: 1 }
+  return houseDb.addHouse(house, testDb)
+    .then(ids => {
+      expect(ids).toHaveLength(1)
+      expect(typeof ids[0]).toBe('number')
+      return null
+    })
+})
+
+test('getHouseById should return house with that id', () => {
+  const id = 4
+  return houseDb.getHouseById(id, testDb)
+    .then(house => {
+      expect(house[0].name).toBe('Keke')
+      expect(house).toHaveLength(1)
       return null
     })
 })
