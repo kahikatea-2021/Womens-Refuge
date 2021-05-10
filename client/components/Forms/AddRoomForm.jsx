908ac6f912669bc78766906a1513101c5032009c
Rebecca-Hallas <rebeccajhallas@gmail.com>
2021-05-11 07:19:34 +1200

    more styling and fresh seed run

--- client/components/Forms/AddRoomForm.jsx
+++ client/components/Forms/AddRoomForm.jsx
@@ -5,6 +5,7 @@ import { Link, useParams } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 
 function AddRoomForm () {
+  const regions = useSelector(state => state.regions)
   const [addedRooms, setAddedRooms] = useState([])
   const { register, handleSubmit, watch, formState: { errors } } = useForm()
   const house = useSelector(state => state.house)
@@ -38,13 +39,10 @@ function AddRoomForm () {
       <p className="text-center text-3xl md:text-5xl font-bold my-1 md:my-2">{house.name} House</p>
         <br /> 
 
-        {/* <div className='text-base md:text-xl'>
-          <ol>
-            <b>Required Information:</b>
-        <li>- the number of rooms available in the house; and</li>
-        <li>- the sleeping arrangements in each room.</li>
-        </ol>
-        </div> */}
+        <p className='text-center text-base md:text-xl'><b>{house.region_id}</b></p>
+        <p className='text-center text-base md:text-xl'><b>{house.phone_1}</b></p>
+        <p className='text-center text-base md:text-xl'><b>{house.phone_2}</b></p>
+        <p className='text-center text-base md:text-xl'><b>{house.notes}</b></p>
 
         <br />
 
@@ -52,22 +50,24 @@ function AddRoomForm () {
 
  <form onSubmit={handleAddroom}>
 
+ {addedRooms.length > 0 && <h1><b>Rooms:</b></h1>}
+        <ul>
+          {addedRooms.map((room, i) => {
+            return <li key={i}>Room {i + 1}: {room.description}</li>
+          })}
+        </ul>
+        <br />
     <input required id='notes' name="description" placeholder='E.g: 1 queen bed, 2 single beds.' value={form.description} onChange={handleChange} type="text" />
 
     <button   className="py-2 ml-4 self-start bg-poroporo hover:bg-poroporo text-white w-16 text-xs rounded-lg focus:ring transform transition hover:scale-105 duration-300 ease-in-out">{addedRooms.length <= 0 ? 'Add First Room' : 'Add Another Room'}</button>
 
     
   <br />
-        {addedRooms.length > 0 && <h1><b>Rooms:</b></h1>}
-        <ul>
-          {addedRooms.map((room, i) => {
-            return <li key={i}>Room {i + 1}: {room.description}</li>
-          })}
-        </ul>
+ 
  
 
         <br />
-        {addedRooms.length <= 0 ? '' : <button className="m-2 py-4 md:w-1/3 w-2/3 self-center bg-poroporo hover:bg-poroporo text-white text-lg rounded-lg focus:ring transform transition hover:scale-105 duration-300 ease-in-out"><Link to={`/house/${house.name}`}>Submit</Link></button>}
+        {addedRooms.length <= 0 ? '' : <button className="m-2 py-4 md:w-1/3 w-2/3 self-center bg-poroporo hover:bg-poroporo text-white text-lg rounded-lg focus:ring transform transition hover:scale-105 duration-300 ease-in-out"><Link className="self-center" to={`/house/${house.name}`}>Submit</Link></button>}
   
         </form>
         </div>
