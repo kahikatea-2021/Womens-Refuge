b62695d6a9a2b5ddcce312808ebd7ef1bbcf327e
Rebecca Hallas <77651862+rebecca-hallas@users.noreply.github.com>
2021-05-10 14:58:17 +1200

    add house form now requires text in room description field

--- client/components/Forms/AddRoomForm.jsx
+++ client/components/Forms/AddRoomForm.jsx
@@ -2,9 +2,11 @@ import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { addRoom } from '../../apis/rooms'
 import { Link, useParams } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 
 function AddRoomForm () {
   const [addedRooms, setAddedRooms] = useState([])
+  const { register, handleSubmit, watch, formState: { errors } } = useForm()
   const house = useSelector(state => state.house)
   const [form, setForm] = useState({
     house_id: house.id,
@@ -17,10 +19,13 @@ function AddRoomForm () {
     setForm({ ...form, [name]: value })
   }
 
-  function handleAddroom () {
+  function handleAddroom (e) {
+    console.log('hi')
+    e.preventDefault()
     addRoom(form)
       .then(() => {
         setAddedRooms([...addedRooms, form])
+        setForm({ ...form, description: '' })
         return null
       })
       .catch(err => console.log(err))
@@ -30,14 +35,15 @@ function AddRoomForm () {
     <>
       {!house.name ? <p>No House Selected</p> : <div>
         <h1>ADD A ROOM</h1>
-        <form>
+        <form onSubmit={handleAddroom}>
           <label htmlFor='house'>House:</label>
           <input readOnly isrequired='true' id='house' value={house.name} type='text'></input>
           <label htmlFor='description'>Description:</label>
-          <textarea isrequired='true' id='notes' name="description" placeholder='E.g: 1 queen bed, 2 single beds.' value={form.description} onChange={handleChange}></textarea>
-          <button type='button' onClick={handleAddroom}>{addedRooms.length <= 0 ? 'ADD ROOM' : 'ADD ANOTHER'}</button>
-          <Link to={`/house/${house.name}`} ><button type='button'>FINISHED</button></Link>
+          <input required id='notes' name="description" placeholder='E.g: 1 queen bed, 2 single beds.' value={form.description} onChange={handleChange}></input>
+          <button >{addedRooms.length <= 0 ? 'ADD ROOM' : 'ADD ANOTHER'}</button>
+
         </form>
+        <Link to={`/house/${house.name}`} ><button type='button'>FINISHED</button></Link>
         {addedRooms.length > 0 && <h1>Rooms Added</h1>}
         <ul>
           {addedRooms.map((room, i) => {
