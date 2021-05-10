5c458194275a91780bb04d77e40b53ab775c0b34
yuan tan <dyxtan@gmail.com>
2021-05-10 20:09:45 +1200

    add check marks to houses indicating if room or not

--- client/components/Region.jsx
+++ client/components/Region.jsx
@@ -31,13 +31,21 @@ function Region () {
   if (isAuthenticated && user) {
     return (
       <>
-        <div className='mt-36'>
-          <h1 className=' flex justify-center font-extrabold text-2xl my-8 mt-20'>Houses</h1>
+        <div className=''>
+          <div className='flex flex-col ml-10 md:flex-row md:justify-center md:ml-1'>
+            <div className='flex items-center mx-5'>
+              <img src="/images/tickGreen.png" className="h-8 w-8 mr-2" alt="" /><p>Room available</p>
+            </div>
+            <div className='flex items-center mx-5'>
+              <img src="/images/crossRed.png" className="h-8 w-8 mr-2" alt="" /><p>No room available</p>
+            </div>
+          </div>
+          <h1 className=' flex justify-center font-extrabold text-2xl my-8 mt-20'>{regionName + ' Houses'}</h1>
           {houses.length > 0 && houses.map(house => {
             return <div key={house.name}><Link className=' flex justify-center ' to={`/house/${house.name}`} >
-              <div className="text-center m-2 py-4 w-2/3 md:w-1/3 self-center bg-poroporo hover:bg-poroporo text-white text-lg rounded-lg focus:ring transform transition hover:scale-105 duration-300 ease-in-out">
-                {house.name}
-              </div>
+              <button className="px-5 flex justify-between items-center text-center m-2 py-4 w-2/3 md:w-1/3 self-center bg-poroporo hover:bg-poroporo text-white text-lg rounded-lg focus:ring transform transition hover:scale-105 duration-300 ease-in-out">
+                <p className="w-8"></p>{house.name} <img src={house.rooms_available > 0 ? '/images/tickWhite.png' : '/images/crossWhite.png'} className="h-8 w-8" alt="" />
+              </button>
             </Link>
             </div>
           })}
