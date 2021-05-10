3d0f221493be1d694d3fbb53b98dce0fd807e2cc
yuan tan <dyxtan@gmail.com>
2021-05-10 19:13:14 +1200

    pre merge with tailwind

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -15,7 +15,7 @@ function Home () {
       {isAuthenticated &&
         <div>
           <div>
-            <h1 className='flex justify-center font-extrabold my-8 mt-20 text-2xl'>Safehouse Search Options:</h1>
+            <h1 className='flex justify-center font-extrabold my-8 mt-20 text-xl md:text-2xl'>Safehouse Search Options:</h1>
             <NorthIslandButton />
             <SouthIslandButton />
           </div>
