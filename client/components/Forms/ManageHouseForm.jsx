49b81a6aa406f2359a208bacbfd82d7d7b19d30c
Karol Bajczyk <75716163+1Karol1@users.noreply.github.com>
2021-05-11 17:29:13 +1200

    styling manage house form

--- client/components/Forms/ManageHouseForm.jsx
+++ client/components/Forms/ManageHouseForm.jsx
@@ -39,15 +39,17 @@ export default function ManageHouseForm (props) {
   return (
 
     <>
-      <form>
-        <label htmlFor='phone1'>Primary Contact Number: </label>
-        <input id='phone1' name='phone_1' value={form.phone_1} type='text' onChange={handleChange}></input>
-        <label htmlFor='phone2'>Secondary Contact Number: </label>
-        <input id='phone2' name='phone_2' type="text" value={form.phone_2} onChange={handleChange}></input>
-        <label htmlFor='notes'>Notes: </label>
-        <textarea id='notes' name='notes' type="text" value={form.notes} onChange={handleChange}></textarea>
-        <button onClick={handleSubmit}>SUBMIT</button>
-      </form>
+      <div className="flex justify-center flex-col mx-auto w-full  md:justify-start md:w-1/3">
+        <label className="block">
+          <span className="text-gray-700 font-bold ">Primary Contact Number: </span>
+          <input className='rounded-lg w-full mt-2' id='phone1' name='phone_1' value={form.phone_1} type='text' onChange={handleChange}></input>
+        </label>
+        <label className='mt-4' htmlFor='phone2 '>Secondary Contact Number: </label>
+        <input className='rounded-lg mt-2' id='phone2' name='phone_2' type="text" value={form.phone_2} onChange={handleChange}></input>
+        <label className='mt-4' htmlFor='notes'>Notes: </label>
+        <textarea className='mt-2 rounded-lg block w-full' id='notes' name='notes' rows="5" type="text" value={form.notes} onChange={handleChange}></textarea>
+        <button className=' font-bold py-2 md:py-3 md:text-base self-center bg-poroporo hover:bg-poroporo text-white w-20 md:w-32 text-xs rounded-lg focus:ring transform transition hover:scale-105 duration-300 ease-in-out' type='button' onClick={handleSubmit}>SUBMIT</button>
+      </div>
     </>
   )
 }
