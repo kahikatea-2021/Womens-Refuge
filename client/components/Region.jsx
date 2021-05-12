459f0284a2a812a0cbfa480ec96904dc0600895d
yuan tan <dyxtan@gmail.com>
2021-05-13 01:29:52 +1200

    more debug messages

--- client/components/Region.jsx
+++ client/components/Region.jsx
@@ -4,7 +4,7 @@ import { getHousesInRegion } from '../apis/regions'
 import { useAuth0 } from '@auth0/auth0-react'
 
 function Region () {
-  const [houses, setHouses] = useState([])
+  const [houses, setHouses] = useState(null)
   const regionName = useParams().name
   const { isLoading, isAuthenticated, user } = useAuth0()
   const history = useHistory()
@@ -26,8 +26,10 @@ function Region () {
     return <img src="../../images/loading.gif"></img>
   }
 
-  if (houses.length > 0) {
-    console.log(houses[0].name)
+  if (houses) {
+    console.log(houses)
+  } else {
+    console.log('not houses')
   }
 
   if (isAuthenticated && user) {
@@ -42,7 +44,8 @@ function Region () {
           </div>
         </div>
         <h1 className=' flex justify-center font-extrabold text-2xl mb-8 mt-6'>{regionName + ' Houses'}</h1>
-        {houses.length > 0 && houses.map(house => {
+        {}
+        {houses && houses.map(house => {
           return <div key={house.name} className=' flex justify-center '>
             <button onClick={() => { history.push(`/house/${house.name}`) }} className="px-5 flex justify-between items-center text-center m-2 py-4 w-2/3 md:w-1/3 self-center bg-poroporo hover:bg-poroporo text-white text-lg rounded-lg focus:ring transform transition hover:scale-105 duration-300 ease-in-out">
               <p className="w-8"></p>{house.name} <img src={house.available_rooms > 0 ? '/images/tickWhite.png' : '/images/crossWhite.png'} className="w-6 md:w-8" alt="" />
