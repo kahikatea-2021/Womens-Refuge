eb0c2af128eea819ca1adcd853602b2667a5ee19
grace-malae <grace.t.malae@gmail.com>
2021-05-12 19:33:11 +1200

    adjust foreign key constraints on migration

--- server/db/migrations/20210506102809_houses.js
+++ server/db/migrations/20210506102809_houses.js
@@ -1,5 +1,5 @@
-exports.up = function (knex) {
-  return knex.schema.createTable('houses', table => {
+exports.up = async function (knex) {
+  await knex.schema.createTable('houses', table => {
     table.increments('id').primary()
     table.integer('region_id').references('regions.id')
     table.string('name')
@@ -7,8 +7,15 @@ exports.up = function (knex) {
     table.string('phone_2')
     table.string('notes')
   })
+
+  await knex.schema.alterTable('users', table => {
+    table.foreign('house_id').references('houses.id').onDelete('SET NULL')
+  })
 }
 
-exports.down = function (knex) {
-  return knex.schema.dropTable('houses')
+exports.down = async (knex) => {
+  await knex.schema.alterTable('users', table => {
+    table.dropForeign('house_id')
+  })
+  await knex.schema.dropTable('houses')
 }
