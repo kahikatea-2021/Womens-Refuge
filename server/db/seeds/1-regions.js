2768d73dff7b835fb8483bb86e06646e48325f61
yuan tan <dyxtan@gmail.com>
2021-05-13 04:01:51 +1200

    readd id to seed data

--- server/db/seeds/1-regions.js
+++ server/db/seeds/1-regions.js
@@ -1,20 +1,20 @@
 exports.seed = function (knex) {
   // Inserts seed entries
   return knex('regions').insert([
-    { island: 'north', region: 'Northland' },
-    { island: 'north', region: 'Auckland' },
-    { island: 'north', region: 'Bay of Plenty' },
-    { island: 'north', region: 'Waikato' },
-    { island: 'north', region: 'Gisborne' },
-    { island: 'north', region: 'Taranaki' },
-    { island: 'north', region: "Hawke's Bay" },
-    { island: 'north', region: 'Whanganui - Manawatu' },
-    { island: 'north', region: 'Wellington' },
-    { island: 'south', region: 'Nelson' },
-    { island: 'south', region: 'Marlborough' },
-    { island: 'south', region: 'Canterbury' },
-    { island: 'south', region: 'West Coast' },
-    { island: 'south', region: 'Otago' },
-    { island: 'south', region: 'Southland' }
+    { id: 1, island: 'north', region: 'Northland' },
+    { id: 2, island: 'north', region: 'Auckland' },
+    { id: 3, island: 'north', region: 'Bay of Plenty' },
+    { id: 4, island: 'north', region: 'Waikato' },
+    { id: 5, island: 'north', region: 'Gisborne' },
+    { id: 6, island: 'north', region: 'Taranaki' },
+    { id: 7, island: 'north', region: "Hawke's Bay" },
+    { id: 8, island: 'north', region: 'Whanganui - Manawatu' },
+    { id: 9, island: 'north', region: 'Wellington' },
+    { id: 10, island: 'south', region: 'Nelson' },
+    { id: 11, island: 'south', region: 'Marlborough' },
+    { id: 12, island: 'south', region: 'Canterbury' },
+    { id: 13, island: 'south', region: 'West Coast' },
+    { id: 14, island: 'south', region: 'Otago' },
+    { id: 15, island: 'south', region: 'Southland' }
   ])
 }
