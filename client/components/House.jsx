910b0771522c37a95971b5886a6d00044c44b997
yuan tan <dyxtan@gmail.com>
2021-05-13 02:46:52 +1200

    redid button logic to work for both postgre and sqlite

--- client/components/House.jsx
+++ client/components/House.jsx
@@ -62,6 +62,17 @@ function House () {
       .catch(err => console.log(err))
   }
 
+  function isAdminOrHouseCor () {
+    if (ourUser && house[0]) {
+      if (ourUser.isAdmin || (ourUser.house_id === house[0].house_id)) return true
+    }
+    return false
+  }
+
+  function isAdmin () {
+    return !!((ourUser && ourUser.isAdmin))
+  }
+
   return (
     <>
       {house &&
@@ -70,9 +81,9 @@ function House () {
             <p className='text-center text-base md:text-xl'><b>{house[0].region}</b></p>
             <div className="flex items-center justify-center flex-col md:flex-row">
               {/* invisible button to match the visible one to center the name */}
-              {(ourUser?.isAdmin === true || ourUser?.house_id === house[0].house_id) && <div className="invisible hidden md:block mx-2"><ManageHouseButton text='EDIT' path={house[0].house_id} /></div>}
+              {isAdminOrHouseCor() && <div className="invisible hidden md:block mx-2"><ManageHouseButton text='EDIT' path={house[0].house_id} /></div>}
               <p className="text-center text-3xl md:text-5xl font-bold">{house[0].name}</p>
-              {(ourUser?.isAdmin === true || ourUser?.house_id === house[0].house_id) && <div className="mx-2"><ManageHouseButton text='EDIT' path={house[0].house_id} /></div>}
+              {isAdminOrHouseCor() && <div className="mx-2"><ManageHouseButton text='EDIT' path={house[0].house_id} /></div>}
             </div>
             <div className='flex justify-center m-4'>{count > 0 && <div className="flex flex-row">available<img className='ml-2 w-6' src='/images/tickGreen.png' /></div>}</div>
             <div className='flex justify-center m-4'>{count === 0 && <div className="flex flex-row">unavailable<img className='ml-2 w-6' src='/images/crossRed.png' /></div>}</div>
@@ -99,7 +110,7 @@ function House () {
               })}
             </div>
             {house[0]?.notes && <div className='text-base md:text-xl'><b>Additional Information:</b><br />{house[0].notes}</div>}
-            {(ourUser && ourUser.isAdmin === true) && <div className="flex flex-col justify-center items-center">
+            {isAdmin() && <div className="flex flex-col justify-center items-center">
               {!confirmDelete ? <DeleteHouseButton text={confirmDelete ? 'Confirm' : null} callback={setConfirmDelete} /> : <p>Confirm Delete</p>}
               {(confirmDelete && !deleted) && <div className="flex mt-2">
                 <DeleteHouseButton text="YES" callback={handleDelete} />
