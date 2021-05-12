09316fc8a959f15dddcab7b11c622ac4c7b09675
1Karol1 <kbajczyk@gmail.com>
2021-05-12 17:58:56 +1200

    component tests fixed

--- client/components/Buttons/LogoutButton.test.js
+++ client/components/Buttons/LogoutButton.test.js
@@ -1,46 +1,44 @@
 import React from 'react'
-import LogoutButton from  './LogoutButton'
+import LogoutButton from './LogoutButton'
 import { renderWithRedux } from '../../test-utils'
 import { screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 describe('Back Button', () => {
-    it('displays for admin', () => {
-      renderWithRedux(<LogoutButton />, 
-        { 
-          initialState: {
-            user: {
-                isAdmin: true
-              }
-          }
-        })
-      expect(screen.getByRole('button')).toBeInTheDocument()
-    })
-  
-    it('displays for everyone', () => {
-      renderWithRedux(<LogoutButton />, 
-        { 
-          initialState: {
-            user: {
-                isRefugeCoordinator: true
-              }
-          }
-        })
-      expect(screen.getByRole('button')).toBeInTheDocument()
-    })
-
-    it('displays for generic user', () => {
-      renderWithRedux(<LogoutButton />, 
-        { 
-          initialState: {
-            user: {
-              isRefugeCoordinator: false,
-              isAdmin: false
+  it('displays for admin', () => {
+    renderWithRedux(<LogoutButton />,
+      {
+        initialState: {
+          user: {
+            isAdmin: true
           }
         }
-        })
-      expect(screen.getByRole('button')).toBeInTheDocument()
-    })
-
-  
+      })
+    expect(screen.getByRole('button')).toBeInTheDocument()
   })
+
+  it('displays for everyone', () => {
+    renderWithRedux(<LogoutButton />,
+      {
+        initialState: {
+          user: {
+            isRefugeCoordinator: true
+          }
+        }
+      })
+    expect(screen.getByRole('button')).toBeInTheDocument()
+  })
+
+  it('displays for generic user', () => {
+    renderWithRedux(<LogoutButton />,
+      {
+        initialState: {
+          user: {
+            isRefugeCoordinator: false,
+            isAdmin: false
+          }
+        }
+      })
+    expect(screen.getByRole('button')).toBeInTheDocument()
+  })
+})
